874796541fa258341f3dae1d5b79befa79d141a5
Ibrahim Abiola <ibrahimabiola75@yahoo.com>
2022-05-03 03:33:45 +0100

    initial commit

---
+++ app/Models/Project.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'introduction',
+        'created_at',
+        'location',
+        'cost'
+    ];
+
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $table = 'projects';
+    public $timestamps = true;
+
+    protected $casts = [
+        'cost' => 'float'
+    ];
+}
